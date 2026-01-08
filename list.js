0ead21c61f0917b9bb421704f84a3e5e6597a4c1
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-08 19:49:05 +0530

    Redis list operations

---
+++ list.js
@@ -0,0 +1,9 @@
+import client from "./client.js";
+
+// console.log(await client.lrange("messages" , 0 , -1));
+
+// console.log(await client.blpop("messages" , 5));
+
+// console.log(await client.del("msg"));
+
+
