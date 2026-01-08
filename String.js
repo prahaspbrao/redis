32aa8b973f638e15da4552d834ba3593528742b2
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-08 19:27:10 +0530

    Client interaction lpush , lpop etc

--- String.js
+++ String.js
@@ -1,9 +1,17 @@
 import client from "./client.js";
 
-async function init() {
-    await client.set("msg:6" , "Hey from Node js");
-    const result = await client.get("msg:6");
-    console.log("Result => ", result);
-}
+await client.lpush("messages", "A"); // ["A"]
+await client.rpush("messages", "B"); // ["A", "B"]
+await client.lpush("messages", "C"); // ["C", "A", "B"]
 
-init();
+console.log(await client.lrange("messages", 0, -1));
+// ["C", "A", "B"]
+
+console.log(await client.llen("messages"));
+
+console.log(await client.lpop("messages")); // "C"
+console.log(await client.rpop("messages")); // "B"
+
+console.log(await client.llen("messages"));
+
+
